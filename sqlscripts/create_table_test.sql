53c6bbadd861fe9ff56fb02e6138e5519942f178
Sam Phippen <samphippen@googlemail.com>
2012-07-21 16:10:06 +0100

    Fix output formatting
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- sqlscripts/create_table_test.sql
+++ sqlscripts/create_table_test.sql
@@ -1,2 +1,2 @@
 drop table test;
-CREATE TABLE test (id INTEGER AUTO INCREMENT, artist INTEGER, track INTEGER, user INTEGER, time INTEGER);
+CREATE TABLE test (id INTEGER AUTO INCREMENT, artist INTEGER, track INTEGER, user INTEGER, time INTEGER, oc INTEGER);
