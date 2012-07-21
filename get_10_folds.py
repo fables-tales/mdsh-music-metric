6655a05619122c155b50b8083e4c96b07d0929f3
Sam Phippen <samphippen@googlemail.com>
2012-07-21 13:37:51 +0100

    First submission code
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- get_10_folds.py
+++ get_10_folds.py
@@ -7,8 +7,10 @@ import cPickle as pickle
 import recsys.algorithm
 
 recsys.algorithm.VERBOSE = True
-from recsys.algorithm.factorize import SVD
+from recsys.algorithm.factorize import SVD, SVDNeighbourhood
 from recsys.evaluation.prediction import RMSE, MAE
+
+import random
 
 
 libmc = True
@@ -59,16 +61,108 @@ def tenFolds():
 
 
 
-def collab_filter():
+def collab_filter(model, filename):
     conn = sqlite3.connect("db.sqlite")
     cur = conn.cursor()
-    l = list(cur.execute("SELECT user,track,rating FROM train"))
+    l = list(cur.execute("SELECT rating,track,user FROM train"))
     K = 100
-    svd = SVD()
+    svd = model
     svd.set_data(l)
-    svd.compute(k=K, min_values=0.1, pre_normalize=None, mean_center=True, post_normalize=True)
-    svd.save_model("svd.model")
-    
+    svd.compute(k=K, min_values=0.0, pre_normalize=None, mean_center=False, post_normalize=True)
+    svd.save_model(filename)
+
+def test_classifier(model, filename):
+    conn = sqlite3.connect("db.sqlite")
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    cur.execute("SELECT count(*) fROM train")
+    c = cur.fetchone()
+    print c
+    svd = model
+    svd.load_model(filename)
+    l = cur.execute("SELECT * FROM train")
+    pairs = []
+    for idx,item in enumerate(l): 
+        user = item["user"]
+        track = item["track"]
+        if idx % 1024 == 0:
+            print idx, c
+        pairs.append((svd.predict(track,user), item["rating"]))
+
+    print RMSE(pairs).compute()
+
+
+def track_averages():
+    conn = sqlite3.connect("db.sqlite")
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    cur.execute("SELECT avg(rating),track fROM train group by track")
+    scores = {}
+    for row in cur:
+        scores[row["track"]] = row["avg(rating)"]
+    return scores
+
+def user_averages():
+    conn = sqlite3.connect("db.sqlite")
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    cur.execute("SELECT avg(rating),user fROM train group by user")
+    scores = {}
+    for row in cur:
+        scores[row["user"]] = row["avg(rating)"]
+    return scores
+
+
+
+def classify_test_set(model,filename):
+    conn = sqlite3.connect("db.sqlite")
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    cur.execute("SELECT count(*) fROM test")
+    c = cur.fetchone()
+    print c
+    svd = model
+    svd.load_model(filename)
+    l = cur.execute("SELECT * FROM test")
+    pairs = []
+    track_means = track_averages()
+    user_means  = user_averages()
+    build = ""
+    for idx,item in enumerate(l): 
+        user = item["user"]
+        track = item["track"]
+        if idx % 1024 == 0:
+            print idx, c
+        print track
+        print user
+        try:
+            r = svd.predict(track,user) 
+        except (KeyError), err:
+            if (err == user):
+                r = track_means[track]
+            elif (err == track):
+                r = user_means[user]
+            else:
+                r = 50
+            
+        if r > 100:
+            r = 100
+        if r < 0:
+            r = 0
+        
+        build += str(item["artist"]) + "," + str(item["track"]) + "," + str(item["user"]) +  "," + str(r) + "\n"
+
+
+    f = open("answer.csv", "w")
+    f.write(build)
+    f.flush()
+    f.close()
+
 
 if __name__ == "__main__":
-    collab_filter()
+    test_classifier(SVD(), "svd.model")
+    #x = SVDNeighbourhood()
+    #collab_filter(x, "svd-n.model")
+    #print "bees"
+    #test_classifier(x, "svd-n.model")
+    classify_test_set(SVD(), "svd.model")
