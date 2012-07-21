0cbe99f9f2381a1f3ff3bcc25714a7ee309929ad
Sam Phippen <samphippen@googlemail.com>
2012-07-21 13:49:11 +0100

    Refactoring and make everything error average of all track ratings
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- get_10_folds.py
+++ get_10_folds.py
@@ -71,6 +71,43 @@ def collab_filter(model, filename):
     svd.compute(k=K, min_values=0.0, pre_normalize=None, mean_center=False, post_normalize=True)
     svd.save_model(filename)
 
+
+def everything_average():
+    conn = sqlite3.connect("db.sqlite")
+    cur = conn.cursor()
+    cur.execute("SELECT avg(rating) from train")
+    return cur.fetchone()[0]
+
+user_fallbacks = 0
+track_fallbacks = 0
+everything_fallbacks = 0
+
+def predict_item(svd, track, user):
+    global everything_fallbacks
+    global track_fallbacks
+    global user_fallbacks
+    try:
+        r = svd.predict(track,user) 
+    except (KeyError), err:
+        if (err == user):
+            r = track_means[track]
+            user_fallbacks += 1
+        elif (err == track):
+            r = user_means[user]
+            track_fallbacks += 1
+        else:
+            r = everything_average()
+            everything_fallbacks += 1
+        
+    if r > 100:
+        r = 100
+    if r < 0:
+        r = 0
+
+    return r
+
+
+
 def test_classifier(model, filename):
     conn = sqlite3.connect("db.sqlite")
     conn.row_factory = dict_factory
@@ -87,7 +124,7 @@ def test_classifier(model, filename):
         track = item["track"]
         if idx % 1024 == 0:
             print idx, c
-        pairs.append((svd.predict(track,user), item["rating"]))
+        pairs.append((predict_item(svd, track,user), item["rating"]))
 
     print RMSE(pairs).compute()
 
@@ -133,23 +170,8 @@ def classify_test_set(model,filename):
         track = item["track"]
         if idx % 1024 == 0:
             print idx, c
-        print track
-        print user
-        try:
-            r = svd.predict(track,user) 
-        except (KeyError), err:
-            if (err == user):
-                r = track_means[track]
-            elif (err == track):
-                r = user_means[user]
-            else:
-                r = 50
-            
-        if r > 100:
-            r = 100
-        if r < 0:
-            r = 0
         
+        r = predict_item(svd, track, user)
         build += str(item["artist"]) + "," + str(item["track"]) + "," + str(item["user"]) +  "," + str(r) + "\n"
 
 
@@ -166,3 +188,6 @@ if __name__ == "__main__":
     #print "bees"
     #test_classifier(x, "svd-n.model")
     classify_test_set(SVD(), "svd.model")
+    print "missing users", user_fallbacks
+    print "missing track", track_fallbacks
+    print "all missing"  , everything_fallbacks
