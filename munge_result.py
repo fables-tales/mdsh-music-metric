0f03e11697cde433f4b62934dd91ba8f112bbe49
Ben Fields <me@benfields.net>
2012-07-22 11:41:25 +0100

    does a merge

---
+++ munge_result.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+munge_result.py
+
+Created by Benjamin Fields on 2012-07-22.
+Copyright (c) 2012 . All rights reserved.
+"""
+
+import sys
+import getopt
+import simplejson
+
+
+help_message = '''
+The help message goes here.
+'''
+
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def main(argv=None):
+    with open(sys.argv[1]) as wh:
+        good_res = simplejson.load(wh)
+    with open(sys.argv[2]) as wh:
+        fallback = wh.readlines()
+    output = [None]*len(fallback)
+    for thing in good_res:
+        output[thing['order']] = thing['score']
+    for idx in xrange(len(fallback)):
+        if not output[idx]:
+            output[idx] = fallback[idx]
+    with open(sys.argv[3], 'w') as oh:
+        oh.write('\n'.join(output))
+        oh.write('\n')
+        
+
+
+if __name__ == "__main__":
+    sys.exit(main())
