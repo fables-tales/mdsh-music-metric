c68afde18a594b885d31e32f8bd1008fbd214a79
Ben Fields <me@benfields.net>
2012-07-22 11:41:39 +0100

    now with saving

--- regress.py
+++ regress.py
@@ -1,4 +1,5 @@
 import sqlite3
+import simplejson
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, RandomForestRegressor
 
 
@@ -17,6 +18,7 @@ words = "Uninspired,Sophisticated,Aggressive,Edgy,Sociable,Laid back,Wholesome,U
 useful_keys = ["q_" + i for i in "abcdefghijklmnopqrs"] + ["age"] + [x.lower().replace(" ", "_") for x in words.split(",")] + ["user","artist","time"]
 
 if __name__ == "__main__":
+    wh = open('regressionresult.json', 'w')
     cur = conn.cursor()
     q1 = "SELECT * FROM users u, train t, raw_words r WHERE u.user = r.user AND u.user = t.user AND t.artist = r.artist;"
     values_arr = []
@@ -42,26 +44,35 @@ if __name__ == "__main__":
 
     
 
-    trn_v = values_arr[0:int(len(values_arr)*0.7)]
+    trn_v = values_arr
     print "bees"
-    trn_l = labels_arr[0:int(len(labels_arr)*0.7)]
+    trn_l = labels_arr
     print "fitting"
     clf.fit(trn_v, trn_l)
 
     diff = 0
     k = 0
 
-    print "predicting"
-    cls = clf.predict(values_arr)
-    for i in range(int(len(values_arr)*0.7),len(values_arr)):
-        if cls[i]>100:
-            pred = 100.0
-        elif cls[i]<0:
-            pred = 0.0
+    cur = conn.cursor()
+    results = []
+    q1 = "SELECT * FROM users u, test t, raw_words r WHERE u.user = r.user AND u.user = t.user AND t.artist = r.artist;"
+    for idx,x in enumerate(cur.execute(q1)):
+        values = []
+        for key in useful_keys:
+             assert isinstance(x[key], int) or isinstance(x[key], float) or x[key] == ""
+             if x[key] == "":
+                 x[key] = 0
+             values.append(x[key])
+        assert len(values) == len(useful_keys)
+        cls = float(clf.predict(values)[0])   
+        if cls > 100:
+             pred = 100.0
+        elif cls < 0:
+             pred = 0.0
         else:
-            pred = cls[i]
-        d = pred - labels_arr[i]
-        diff += d*d
-        k += 1
-    diff /= k
-    print diff ** 0.5
+             pred = cls
+         
+    results.append({"score":pred,"order":x['oc']})
+    if idx % 1024 == 0:
+         print idx
+    simplejson.dump(wh, results)
